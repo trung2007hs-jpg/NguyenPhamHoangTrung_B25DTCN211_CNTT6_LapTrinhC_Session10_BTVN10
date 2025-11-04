769b8b8a2854c59260fe76a53a35b69a170a2658
trung2007hs-jpg <trung2007hs@gmail.com>
2025-11-05 01:06:41 +0700

    Update Bai_lam_8.c

--- Bai_lam_8.c
+++ Bai_lam_8.c
@@ -2,8 +2,10 @@
 int main() {
     int arr[6] = {5, 1, 8, 3, 2, 7};
     int n = 6;
-    int k = 4;
+    int x = 5; 
     int i, j, temp;
+    int left, right, mid;
+    int found = -1;
     for (i = 0; i < n - 1; i++) {
         for (j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
@@ -18,11 +20,24 @@ int main() {
         printf("%d ", arr[i]);
     }
     printf("\n");
-    if (k >= 1 && k <= n) {
-        printf("Phan tu thu can tim la: %d\n", k, arr[k - 1]);
-    } else {
-        printf("Khong co phan tu thu %d trong mang.\n", k);
+    left = 0;
+    right = n - 1;
+    while (left <= right) {
+        mid = (left + right) / 2;
+        if (arr[mid] == x) {
+            found = mid;
+            break;
+        } else if (arr[mid] < x) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
     }
+    if (found != -1)
+        printf("Chi so cua phan tu can tim: %d\n", found);
+    else
+        printf("Khong tim thay phan tu trong mang.\n");
     return 0;
 }
+
 
